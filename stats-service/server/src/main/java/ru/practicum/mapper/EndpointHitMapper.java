d761a2f02b01676ee0d626481aa53cb96291a654
Polina <cherkaschenkopo@gmail.com>
2023-09-23 16:26:56 +0300

    Этап 1. Сервис статистики

---
+++ stats-service/server/src/main/java/ru/practicum/mapper/EndpointHitMapper.java
@@ -0,0 +1,26 @@
+package ru.practicum.mapper;
+
+import ru.practicum.EndpointHitDto;
+import ru.practicum.model.EndpointHit;
+
+public class EndpointHitMapper {
+    public static EndpointHitDto toEndpointHitDto(EndpointHit endpointHit) {
+        return new EndpointHitDto(
+                endpointHit.getId(),
+                endpointHit.getApp(),
+                endpointHit.getUri(),
+                endpointHit.getIp(),
+                endpointHit.getTimestamp()
+        );
+    }
+
+    public static EndpointHit toEndpointHit(EndpointHitDto endpointHitDto) {
+        return new EndpointHit(
+                endpointHitDto.getId(),
+                endpointHitDto.getApp(),
+                endpointHitDto.getUri(),
+                endpointHitDto.getIp(),
+                endpointHitDto.getTimestamp()
+        );
+    }
+}
